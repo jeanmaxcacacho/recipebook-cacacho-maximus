38854f962d2fa2bb74ba4d69c1a3686defc83ff0
jeanmaximuscacacho <jean.cacacho@student.ateneo.edu>
2024-02-15 14:43:45 +0800

    defined views in views.py

--- ledger/views.py
+++ ledger/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+def recipe_list(request):
+    return render(request, 'ledger/recipe_list.html')
+
+
+def recipe_1(request):
+    return render(request, 'ledger/recipe_1.html')
+
+
+def recipe_2(request):
+    return render(request, 'ledger/recipe_2.html')
