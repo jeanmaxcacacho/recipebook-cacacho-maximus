80921a529d3c77fceef8c674d4f8cb0dec3f47b0
jeanmaximuscacacho <jean.cacacho@student.ateneo.edu>
2024-03-01 10:39:58 +0800

    defined __str__ methods for Ingredient and Recipe

--- ledger/models.py
+++ ledger/models.py
@@ -7,8 +7,16 @@ class Ingredient(models.Model):
     name = models.charField(max_length=100)
 
 
+    def __str__(self):
+        return self.name
+
+
 class Recipe(models.Model):
     name = models.charField(max_length=100)
+
+
+    def __str__(self):
+        return self.name
 
 
 class RecipeIngredient(models.Model):
