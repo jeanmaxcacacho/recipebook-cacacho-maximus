95cb658eb4d8cfcc3b68bacf1b5e25e9e0dba4ca
jeanmaximuscacacho <jean.cacacho@student.ateneo.edu>
2024-03-03 21:35:36 +0800

    model cleaning

--- ledger/models.py
+++ ledger/models.py
@@ -3,7 +3,16 @@ from django.urls import reverse
 
 # Create your models here.
 
+"""
+https://stackoverflow.com/questions/2642613/what-is-related-name-used-for
+https://www.youtube.com/watch?v=qJUgC4T5e_E
+"""
 
+# only two paths in ledger.urls and max 3 templates
+# we'll have og index view, list of all recipes
+# then have detail view of each recipe where only list of one recipe is rendered
+
+# done
 class Ingredient(models.Model):
     name = models.CharField(max_length=100)
 
@@ -11,26 +20,21 @@ class Ingredient(models.Model):
     def __str__(self):
         return self.name
 
-    
-    def get_absolute_url(self): # url to what?
-        return reverse('', args=[str(self.name)])
-
-
+# both views will use this model
+# this model accesses RecipeIngredient to get recipe and the ingredients associated with that recipe
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
+    ingredients = models.CharField(max_length=100)
 
 
     def __str__(self):
         return self.name
 
 
-    def get_absolute_url(self):
-        return reverse('', args=[str(self.name)])
-
-
+# this model will be the "joining table"
 class RecipeIngredient(models.Model):
     quantity = models.IntegerField()
-    ingredient = models.ForeignKey(  # are there more parameters to add?; idt related_name is relevant since there are no "types" of connected classes
+    ingredient = models.ForeignKey(  
             Ingredient,
             on_delete=models.CASCADE,
             default=1
