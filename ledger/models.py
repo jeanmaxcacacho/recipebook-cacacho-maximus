371ffb47d2f2a35119d566b2c9a6da11b1a8d67d
jeanmaximuscacacho <jean.cacacho@student.ateneo.edu>
2024-03-03 22:04:20 +0800

    implemented many-to-many relationship between Recipe & Ingredient

--- ledger/models.py
+++ ledger/models.py
@@ -6,15 +6,17 @@ from django.urls import reverse
 """
 https://stackoverflow.com/questions/2642613/what-is-related-name-used-for
 https://www.youtube.com/watch?v=qJUgC4T5e_E
+https://www.youtube.com/watch?v=gf2-J9YOMcc
 """
 
 # only two paths in ledger.urls and max 3 templates
 # we'll have og index view, list of all recipes
 # then have detail view of each recipe where only list of one recipe is rendered
 
-# done
+# I mean it says "just needs" not "should have" so............
 class Ingredient(models.Model):
     name = models.CharField(max_length=100)
+    recipes = models.ManyToManyField('Recipe', through='RecipeIngredient')
 
 
     def __str__(self):
@@ -24,7 +26,7 @@ class Ingredient(models.Model):
 # this model accesses RecipeIngredient to get recipe and the ingredients associated with that recipe
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
-    ingredients = models.CharField(max_length=100)
+    ingredients = models.ManyToManyField('Ingredient', through='RecipeIngredient')
 
 
     def __str__(self):
@@ -36,11 +38,9 @@ class RecipeIngredient(models.Model):
     quantity = models.IntegerField()
     ingredient = models.ForeignKey(  
             Ingredient,
-            on_delete=models.CASCADE,
-            default=1
+            on_delete=models.CASCADE
         )
     recipe = models.ForeignKey(
             Recipe,
-            on_delete=models.CASCADE,
-            default=1
+            on_delete=models.CASCADE
         )
