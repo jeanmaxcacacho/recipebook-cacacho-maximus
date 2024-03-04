b63c48f4ac051ffffb48b2cbaffdc5bd04d00454
jeanmaximuscacacho <jean.cacacho@student.ateneo.edu>
2024-03-04 19:19:23 +0800

    holy fuck it works

--- ledger/urls.py
+++ ledger/urls.py
@@ -3,5 +3,7 @@ from .views import *
 
 urlpatterns = [
     path('recipes/list/', recipe_list.as_view(), name='recipe_list'),
-    # path('recipe/<int:id>/', recipe_deatil.as_view(), name='recipe_detail'),
+    path('recipes/<int:pk>/', recipe_detail.as_view(), name='recipe_detail'),
 ]
+
+app_name = 'ledger'
