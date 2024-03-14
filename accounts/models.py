bef8fddf455ab0cd8b5fc074ac89e773f105bda7
maxcacacho <jean.cacacho@student.ateneo.edu>
2024-03-15 03:22:22 +0800

    created Profile model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 # Create your models here.
+
+def bio_length(length_num):
+    if value < 255: raise ValidationError("Bio should be at least 255 characters long.")
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    bio = models.CharField(max_length=500, validators=[bio_length])
