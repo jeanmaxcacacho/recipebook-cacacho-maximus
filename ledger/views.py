3f8183d3f20ed845ce6a90cd0850fd0c0119b7dc
maxcacacho <jean.cacacho@student.ateneo.edu>
2024-03-15 13:59:14 +0800

    added LoginRequiredMixin to recipe_detail view

--- ledger/views.py
+++ ledger/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Recipe
 
 # Create your views here.
@@ -13,6 +14,6 @@ class recipe_list(ListView):
     def get_queryset(self):
         return Recipe.objects.all()
 
-class recipe_detail(DetailView):
+class recipe_detail(LoginRequiredMixin, DetailView):
     template_name = 'ledger/recipe_detail.html'
     model = Recipe
