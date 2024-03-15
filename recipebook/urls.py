76412e47986e4038c545a9a022cdb59d85ac6ddc
maxcacacho <jean.cacacho@student.ateneo.edu>
2024-03-15 14:50:21 +0800

    init login page

--- recipebook/urls.py
+++ recipebook/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')), # adds accounts/login and /logout
     path('', include('ledger.urls'))
 ]
